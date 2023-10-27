a4cc7e9223a2af79cde1e1bcaa5c82aed5707f10
Thomas Dang <thong.dang0903@gmail.com>
2023-10-27 22:12:46 +1100

    Setup: Set up basic app with router

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Route, Routes } from "react-router-dom";
+import "./css/App.css";
+import Password from "./pages/password";
+import Letter from "./pages/Letter";
+
+function App() {
+    return <Routes>
+        <Route path="/" element={<Password />} />
+        <Route path="/letter" element={<Letter />} />
+    </Routes>;
+}
+
+export default App;
