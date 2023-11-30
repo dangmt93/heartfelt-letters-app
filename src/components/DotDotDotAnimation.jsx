7a0b1cbda37028442978f377547260b4e1276706
Thomas Dang <thong.dang0903@gmail.com>
2023-11-30 21:28:00 +1100

    Chore: Move favicon to src/ folder; Fix css link; Add vercel.json

--- src/components/DotDotDotAnimation.jsx
+++ src/components/DotDotDotAnimation.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import "../css/DotDotDotAnimation.css";
+import "../css/dotDotDotAnimation.css";
 
 const DotDotDotAnimation = () => {
     const [animationState, setAnimationState] = useState(0);
